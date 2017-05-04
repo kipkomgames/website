10fe8a9c4500921682beeb904e883304583caf33
aizikdahan <aizikdahan@gmail.com>
2017-05-04 20:44:58 +0300

    ema

--- server.js
+++ server.js
@@ -1,7 +1,49 @@
 var express = require('express')
 var path = require("path")
+var bodyParser = require('body-parser');
+
 var app = express()
 app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'gmail.user@gmail.com',
+        pass: 'yourpass'
+    }
+});
+
+
+app.post("/email",(req,res)=>{
+      // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+        to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world ?', // plain text body
+        html: '<b>Hello world ?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+      
+         res.jsonp({status:"ok"})
+    });
+
+})
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
